be802160eeaaec4270220be6076480ed4d49d79f
Jake Lunaria Flores <90809353+Tsokolatee@users.noreply.github.com>
2023-05-14 16:32:09 +0800

    reworked stc

--- src/pages/Footbar.jsx
+++ src/pages/Footbar.jsx
@@ -5,8 +5,8 @@ export default function Footbar() {
         <>
             <footer className="flex flex-col lg:flex-row justify-center items-center space-y-6 lg:space-y-0 lg:space-x-16 p-6 w-full h-auto bg-[#001D3D] ">
                 <div className="flex flex-row items-center">
-                    <img className="mr-4 w-auto h-16" src="/Logo.png" alt="" />
-                    <img className="w-auto h-12" src="/Banner.png" alt="" />
+                    <img className="mr-4 w-auto h-16" src="Logo.png" alt="" />
+                    <img className="w-auto h-12" src="Banner.png" alt="" />
                 </div>
                 <p className="text-gray-300 text-lg lg:text-center font-semibold">© 2023 QuickSnap. All Rights Reserved.</p>
                 <div className="text-gray-300">
